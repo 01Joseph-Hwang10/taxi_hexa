15f3740c6ef53e112dd7a80260616e88579783a9
dohyeon <dohyeon@unist.ac.kr>
2022-07-20 23:46:58 +0900

    taxi_hexa

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:taxi_hexa/widgets/home/app_bar.dart';
 import 'package:taxi_hexa/widgets/home/bottom_buttons.dart';
 
+import 'package:taxi_hexa/widgets/home/taxi_map.dart';
+
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -10,6 +12,7 @@ class Home extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: CustomAppBar(),
+      body: MapSample(),
       bottomNavigationBar: const BottomButtons(),
     );
   }
