fb331b71b758365eef42f3765775b6f8b5763521
eumppe <pych05@unist.ac.kr>
2022-08-29 22:39:33 +0900

    latlngToAddress

--- lib/widgets/home/add.dart
+++ lib/widgets/home/add.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:taxi_hexa/models/taxi_party.dart';
 import 'package:taxi_hexa/themes/colors.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:uuid/uuid.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:address_search_field/address_search_field.dart';
+
+var uuid = const Uuid();
+LatLng current_LatLng = new LatLng(1.10, 45.50);
+LatLng dest_LatLng = new LatLng(37.498326, 126.998452);
 
 class AddParty extends StatefulWidget {
   AddParty({Key? key}) : super(key: key);
@@ -11,19 +19,17 @@ class AddParty extends StatefulWidget {
   State<AddParty> createState() => _AddPartyState();
 }
 
+String? id;
+
+String? name;
+
+String? destinationAddress;
+
+DateTime? departure;
+
+String? description;
+
 class _AddPartyState extends State<AddParty> {
-  String? id;
-
-  String? name;
-
-  String? destinationAddress;
-
-  List<String>? members;
-
-  DateTime? departure;
-
-  String? description;
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -65,6 +71,7 @@ class _AddPartyState extends State<AddParty> {
               ],
             ),
             TextFormField(
+              initialValue: name,
               decoration: const InputDecoration(
                 hintText: 'name',
               ),
@@ -75,17 +82,9 @@ class _AddPartyState extends State<AddParty> {
                 name = value;
               },
             ),
-            TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'destinationAddress',
-              ),
-              onSaved: (value) {
-                destinationAddress = value!;
-              },
-              onChanged: (value) {
-                destinationAddress = value;
-              },
-            ),
+            Text((dest_addresses != null) //주소
+                ? dest_addresses!.first.name!
+                : 'Address is not found'),
             ElevatedButton(
               onPressed: () {
                 DatePicker.showDateTimePicker(context,
@@ -99,6 +98,7 @@ class _AddPartyState extends State<AddParty> {
               child: Text("출발 시간: " + departure.toString()),
             ),
             TextFormField(
+              initialValue: description,
               decoration: const InputDecoration(
                 hintText: 'description',
               ),
@@ -110,16 +110,24 @@ class _AddPartyState extends State<AddParty> {
               },
             ),
             ElevatedButton(
-              onPressed: () async {
-                DatabaseReference ref = FirebaseDatabase.instance
-                    .ref("taxi_party" + count.toString());
-                await ref.set({
-                  "asd": "i think i dont like this",
-                  "departure": departure.toString(),
-                  "name": name
-                });
-                count++;
-              },
+              onPressed:
+                  (name != '' && destinationAddress != '' && departure != null)
+                      ? () async {
+                          String the_uuid = uuid.v1();
+                          DatabaseReference ref = FirebaseDatabase.instance
+                              .ref("taxi_party" + the_uuid.toString());
+                          await ref.set(TaxiPartyModel(
+                            id: the_uuid,
+                            name: name!,
+                            destination: dest_LatLng,
+                            destinationAddress: await setde(dest_LatLng), // 주소
+                            currentPosition: current_LatLng,
+                            members: [],
+                            departure: departure!,
+                            description: description,
+                          ).toJson());
+                        }
+                      : null,
               child: const Text("submit"),
             )
           ],
@@ -128,5 +136,23 @@ class _AddPartyState extends State<AddParty> {
     );
   }
 
-  int count = 0;
+  List<Placemark>? dest_addresses; //좌표->주소. 처음부터 주소를 찍는 방식으로 변경되면 다 지워도 됨.
+
+  Future<String> setde(LatLng latlng) async {
+    print('aa');
+    bool err = false;
+    try {
+      dest_addresses = await placemarkFromCoordinates(
+          dest_LatLng.latitude, dest_LatLng.longitude);
+    } catch (e) {
+      dest_addresses = [];
+      err = true;
+      print(e);
+    }
+    setState(() {
+      destinationAddress = dest_addresses!.first.toString();
+    });
+    print(latlng);
+    return (err) ? "Address is not found" : destinationAddress!;
+  }
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <address_search_field/address_search_field_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  AddressSearchFieldPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("AddressSearchFieldPlugin"));
 }
